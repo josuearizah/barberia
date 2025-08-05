63221b0e4d6e0ff94a600c77cbd9c6ab1819d80f
Joshuaswift19 <147520210+Joshuaswift19@users.noreply.github.com>
2025-08-05 14:15:30 -0500

    Some Changes

--- run.py
+++ run.py
@@ -7,4 +7,5 @@ with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)), debug=False)
+
