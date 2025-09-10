2c3b9839f45cf7c077f314e8edcda737792a8c17
Joshuaswift19 <147520210+Joshuaswift19@users.noreply.github.com>
2025-09-10 15:35:49 -0500

    algo

--- config.py
+++ config.py
@@ -4,6 +4,9 @@ import os
 class Config:
     # Solo PostgreSQL (leer desde env). Ejemplo:
     # DATABASE_URL=postgresql+psycopg://user:pass@host:5432/dbname
+    print("Configurando la base de datos...", flush=True)
+    print("DATABASE_URL:", os.environ.get('DATABASE_URL'), flush=True)
+
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     CLOUDINARY_CLOUD_NAME = "dsi2yfaew"
